5cc14fdda359d830385a7d69ae1e80b20f1b0750
Mike Piccolo <mfpiccolo@gmail.com>
2015-07-05 12:33:56 -0700

    Working reverse function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
-#[no_mangle]
-pub extern fn sum(x: i32, y: i32) -> i32 {
-  x + y
+fn reverse(s: String) -> String {
+  s.chars().rev().collect()
+}
+
+pub extern fn reverse(s: *const libc::c_char) -> *const libc::c_char {
+  // reverse string and return
 }
 
 #[test]
 fn it_works() {
-  assert!(sum(1, 2) == 3);
+  let string = "Don't use palindrome".to_string();
+  assert!(
+    reverse(string) == "emordnilap esu t'noD"
+  );
 }
