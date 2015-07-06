726271f62d611f1c8699ea9a8950bab9638234b9
Mike Piccolo <mfpiccolo@gmail.com>
2015-07-05 19:13:31 -0700

    Working string manipulation ruby to rust and back

--- scrape.rb
+++ scrape.rb
@@ -4,7 +4,9 @@ module Scrape
   extend FFI::Library
   ffi_lib './target/debug/libscrape.dylib'
 
-  attach_function :sum, [:int32, :int32], :int32
+  attach_function :ruby_reverse, [:string], :string
+  attach_function :concat, [:string, :string], :string
 end
 
-puts Scrape.sum(2, 3) == 5 ? "Gone Dun It!" : "Uhhhh????"
+puts Scrape.ruby_reverse("Don't use palindrome") == "emordnilap esu t'noD" ? "Gone Dun It!" : "Uhhhh????"
+puts Scrape.concat("This is", " a sentence.") == "This is a sentence." ? "Gone Dun It!" : "Uhhhh????"
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,29 @@
-fn reverse(s: String) -> String {
-  s.chars().rev().collect()
+#![feature(libc)]
+#![feature(cstr_to_str)]
+#![feature(cstr_memory)]
+extern crate libc;
+
+use std::ffi::{CStr, CString};
+#[no_mangle]
+pub extern fn ruby_reverse(s: *const libc::c_char) -> *const libc::c_char {
+  let str = ruby_string_to_ref_str(s);
+  let string: String = str.chars().rev().collect();
+  string_to_ruby_string(string)
 }
 
-pub extern fn reverse(s: *const libc::c_char) -> *const libc::c_char {
-  // reverse string and return
+#[no_mangle]
+pub extern fn concat(s1: *const libc::c_char, s2: *const libc::c_char) -> *const libc::c_char {
+    let s1_and_str = ruby_string_to_ref_str(s1);
+    let s2_and_str = ruby_string_to_ref_str(s2);
+    let s1_string = s1_and_str.to_string();
+    let string: String = s1_string + s2_and_str;
+    string_to_ruby_string(string)
 }
 
-#[test]
-fn it_works() {
-  let string = "Don't use palindrome".to_string();
-  assert!(
-    reverse(string) == "emordnilap esu t'noD"
-  );
+fn ruby_string_to_ref_str<'a>(r_string: *const libc::c_char) -> &'a str {
+  unsafe { CStr::from_ptr(r_string) }.to_str().unwrap()
+}
+
+fn string_to_ruby_string(string: String) -> *const libc::c_char {
+  CString::new(string).unwrap().into_ptr()
 }
