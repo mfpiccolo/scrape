c4152d6f62833c09cc5498c6970d429f3c84b7f6
Mike Piccolo <mfpiccolo@gmail.com>
2015-07-15 23:28:18 -0700

    Remove target

--- target/debug/scrape-0c5f308515706b9f.d
+++
@@ -1,4 +0,0 @@
-/Users/mfpiccolo/code/mfpiccolo/scrape/target/debug/scrape-0c5f308515706b9f: src/lib.rs
-
-/Users/mfpiccolo/code/mfpiccolo/scrape/target/debug/scrape-0c5f308515706b9f.d: src/lib.rs
-
