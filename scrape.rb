065cce19eac6ab90a7db2e1185ed7071d036df68
Mike Piccolo <mfpiccolo@gmail.com>
2015-07-21 01:39:05 -0700

    Working struct and array

--- scrape.rb
+++ scrape.rb
@@ -5,15 +5,31 @@ module Scrape
   ffi_lib './target/debug/libscrape.dylib'
 
   class TwoNumbers < FFI::Struct
-    layout :first, :int32,
-           :second, :int32
+    layout :first, :uint32,
+           :second, :uint32
+  end
+
+  class FromRustArray < FFI::Struct
+    layout :len,    :size_t, # dynamic array layout
+           :data,   :pointer #
+
+    def to_a
+      self[:data].get_array_of_string(0, self[:len]).compact
+    end
   end
 
   attach_function :add_struct_vals, [TwoNumbers.by_value], :int32
+  attach_function :add_one_to_vals, [TwoNumbers.by_value], TwoNumbers.by_value
+  attach_function :number_to_char_array, [], FromRustArray.by_value
+  attach_function :print_chars, [], :void
 end
 
 tn = Scrape::TwoNumbers.new
 tn[:first] = 10
 tn[:second] = 20
 
-puts Scrape.add_struct_vals(tn) == 30 ? "Gone Dun It!" : "Uhhhh????"
+puts Scrape.add_struct_vals(tn)          == 30 ? "Wooooo!" : "Booooooo!"
+puts Scrape.add_one_to_vals(tn)[:first]  == 11 ? "Wooooo!" : "Booooooo!"
+puts Scrape.add_one_to_vals(tn)[:second] == 21 ? "Wooooo!" : "Booooooo!"
+p Scrape.number_to_char_array.to_a
+Scrape.print_chars
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,65 @@
+#![feature(libc)]
+#![feature(cstr_memory)]
+extern crate libc;
+
+use std::ffi::CString;
+use std::mem;
+
 pub struct TwoNumbers {
     first: i32,
     second: i32,
 }
 
+impl TwoNumbers {
+    fn plus_one_to_each(self) -> TwoNumbers {
+        let mut tn = self;
+        tn.first += 1;
+        tn.second += 1;
+        tn
+    }
+}
+
+#[no_mangle]
+pub extern fn add_one_to_vals(numbers: TwoNumbers) -> TwoNumbers {
+   numbers.plus_one_to_each()
+}
+
 #[no_mangle]
 pub extern fn add_struct_vals(numbers: TwoNumbers) -> i32 {
     numbers.first + numbers.second
+}
+
+#[repr(C)]
+pub struct RubyArray {
+  len: libc::size_t,
+  data: *const libc::c_void,
+}
+
+impl RubyArray {
+  fn from_vec<T>(vec: Vec<T>) -> RubyArray {
+    let array = RubyArray { data: vec.as_ptr() as *const libc::c_void, len: vec.len() as libc::size_t };
+    mem::forget(vec);
+    array
+  }
+}
+
+#[no_mangle]
+pub extern fn number_to_char_array() -> RubyArray {
+  let mut utf_chars: Vec<*const libc::c_char> = vec![];
+
+  for i in 33..126 {
+    let maybe_char: Option<char> = std::char::from_u32(i);
+    utf_chars.push(CString::new(maybe_char.unwrap().to_string()).unwrap().into_ptr());
+  }
+
+  RubyArray::from_vec(utf_chars)
+}
+
+#[no_mangle]
+pub extern fn print_chars() {
+  for i in 33..126 {
+    println!("{:?}", std::char::from_u32(i).unwrap());
+  }
 }
 
 #[test]
